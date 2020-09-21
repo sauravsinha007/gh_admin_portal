506b95d15bbcba7a2ef441c1f72169e016bbeecc
Saurav <sauravsinha007@gmail.com>
2020-09-22 00:59:18 +0530

    Bug fixes.

--- js/home.js
+++ js/home.js
@@ -1,3 +1,60 @@
 $('#invoice_id').click(function(){
             location.href='invoice.html';
         });
+
+
+
+$('#import_customer_id').click(function(){
+
+		$('input[type=file]').trigger('click');
+       
+    });
+
+
+$('input[type=file]').change(function() {
+    //$('input[type=text]').val($(this).val());
+
+      var lFileName =  document.getElementById("import_file").files[0].name;  //$(this).val()
+      console.log("File Path: " + lFileName);
+
+      processToUploadFileOnServer(lFileName)
+});
+
+
+
+
+function processToUploadFileOnServer(fileName)
+{
+
+    var formData = new FormData();
+    formData.append('customer', $('input[type=file]')[0].files[0]);
+
+	startLoadingIndicatorWithLabel("Loading...");
+    var lUrl = TxConstants.BaseUrl + "/uploadCustomer";
+    console.log("Request Url: " + lUrl);
+    $.ajax({
+           type: "POST",
+           url: lUrl,
+           data: formData,
+           contentType: false,
+           processData: false,
+           success:function(responseStr){
+           stopLoadingIngicator();
+           console.log("Response" +JSON.stringify(responseStr));
+           //{"Success":1,"Message":"Customer Data inserted"}
+           if(responseStr.Success == 1) {
+           		alert("Customer Data Successfully inserted.");
+           }
+           else {
+				alert("Message : " + responseStr.Message);
+           }
+
+           },
+           error:  function(errorStr){
+           stopLoadingIngicator();
+           alert("Error : " +JSON.stringify(errorStr));
+           //navigator.notification.alert(TxConstants.ServerErrorMsg, clickOnOKAlertBtn, '', 'OK');
+           }
+           });
+   
+}
